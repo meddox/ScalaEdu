a7bf3470b1c189cad2f6f76d1ad7aa6c7879e6f4
unknown <meddox@.(none)>
2011-02-20 19:05:32 +0100

    toto je muj prvni commit a moje prvni commit message

--- src/main/scala/com/meddoxdev/edu/App.scala
+++ src/main/scala/com/meddoxdev/edu/App.scala
@@ -8,7 +8,7 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
+    println( "Hi my lovely World!" )
     println("concat arguments = " + foo(args))
   }
 
---
+++ src/main/scala/com/meddoxdev/edu/pokus.scala
@@ -0,0 +1,11 @@
+package main.scala.com.meddoxdev.edu
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: meddox
+ * Date: 19.2.11
+ * Time: 22:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+class pokus
